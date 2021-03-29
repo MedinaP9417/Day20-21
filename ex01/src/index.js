88dbec527193f21545a2e1315da2d13f1fa014d7
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-29 03:57:32 +0200

    Added Project corrections for Day20-21

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -170,6 +170,10 @@ const Keyboard = {
     },
 };
 
+function myFunction(rock) {
+    document.getElementById("virtual-keyboard").innerHTML += rock;
+    console.log(rock);
+}
 
 window.addEventListener("DOMContentLoaded", function() {
     Keyboard.init();
